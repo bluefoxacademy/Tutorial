c314495538a4b9b830ab5ded4ad3e3c124a73632
dotnetsatya <dotnetsatya@gmail.com>
2016-11-10 20:38:57 -0500

    Angularjs Day 1
    Angularjs Day 1

---
+++ Angular/AngularApp/App/app.js
@@ -0,0 +1,21 @@
+﻿var myApp = angular.module('myApp', []);
+
+myApp.controller('productController', function ($scope) {
+    $scope.isAddOrEdit = false;
+    $scope.products = [{ productId: 101, productName: 'Laptop', price: 1000 }, { productId: 102, productName: 'Mac', price: 2000 }];
+    $scope.newProduct = {};
+
+    $scope.addNew = function () {
+        $scope.isAddOrEdit = true;
+    }
+
+    $scope.cancel = function () {
+        $scope.isAddOrEdit = false;
+    }
+
+    $scope.saveProduct = function () {
+        $scope.products.push($scope.newProduct);
+        $scope.newProduct = {};
+        $scope.cancel();
+    }
+});

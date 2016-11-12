84b8f8333f830095b20aeab155e0dd3497c2af0d
dotnetsatya <dotnetsatya@gmail.com>
2016-11-11 20:48:06 -0500

    Angular day 2
    Angular day 2

--- Angular/AngularApp/App/app.js
+++ Angular/AngularApp/App/app.js
@@ -1,14 +1,28 @@
 ﻿var myApp = angular.module('myApp', []);
 
 myApp.controller('productController', function ($scope) {
+    $scope.pages = [];
+    $scope.pageSize = 5;
     $scope.isAddOrEdit = false;
-    $scope.products = [{ productId: 101, productName: 'Laptop', price: 1000 }, { productId: 102, productName: 'Mac', price: 2000 }];
+    $scope.todayDate = new Date();
+    $scope.products = [{ productId: 101, productName: 'Laptop', price: 500, category: "computer" }, { productId: 102, productName: 'Mac', price: 2000, category: "computer" }, { productId: 103, productName: 'Mouse', price: 1000, category: "Hardware" }, { productId: 101, productName: 'Laptop', price: 500, category: "computer" }, { productId: 102, productName: 'Mac', price: 2000, category: "computer" }, { productId: 103, productName: 'Mouse', price: 1000, category: "Hardware" }, { productId: 101, productName: 'Laptop', price: 500, category: "computer" }, { productId: 102, productName: 'Mac', price: 2000, category: "computer" }, { productId: 103, productName: 'Mouse', price: 1000, category: "Hardware" }, { productId: 101, productName: 'Laptop', price: 500, category: "computer" }, { productId: 102, productName: 'Mac', price: 2000, category: "computer" }, { productId: 103, productName: 'Mouse', price: 1000, category: "Hardware" }, { productId: 101, productName: 'Laptop', price: 500, category: "computer" }, { productId: 102, productName: 'Mac', price: 2000, category: "computer" }, { productId: 103, productName: 'Mouse', price: 1000, category: "Hardware" }, { productId: 101, productName: 'Laptop', price: 500, category: "computer" }, { productId: 102, productName: 'Mac', price: 2000, category: "computer" }, { productId: 103, productName: 'Mouse', price: 1000, category: "Hardware" }];
+    $scope.categories = ["Mobile","Computer","Hardware"];
     $scope.newProduct = {};
-
+    $scope.selectedProduct = null;
+    $scope.isNewProduct = true;
     $scope.addNew = function () {
         $scope.isAddOrEdit = true;
+        $scope.isNewProduct = true;
     }
+    var init = function () {
+        var noRecords = $scope.products.length;
+        var noPages = noRecords / $scope.pageSize;
+        for (var i = 1; i <= noPages; i++) {
+            $scope.pages.push(i);
+        }
 
+    }
+    init();
     $scope.cancel = function () {
         $scope.isAddOrEdit = false;
     }
@@ -18,4 +32,49 @@ myApp.controller('productController', function ($scope) {
         $scope.newProduct = {};
         $scope.cancel();
     }
+
+    $scope.viewDetails = function (prod) {
+        $scope.selectedProduct = prod;
+    }
+
+    $scope.editProduct = function (id) {
+        $scope.isNewProduct = false;
+        for (var i = 0; i < $scope.products.length; i++) {
+            if ($scope.products[i].productId == id) {
+                $scope.newProduct = $scope.products[i]
+                break;
+            }
+        }
+        $scope.isAddOrEdit = true;
+    }
+
+    $scope.updateProduct = function () {
+        for (var i = 0; i < $scope.products.length; i++) {
+            if ($scope.products[i].productId == $scope.newProduct.productId) {
+                $scope.products[i].productName = $scope.newProduct.productName;
+                $scope.products[i].price = $scope.newProduct.price;
+                break;
+            }
+        }
+        $scope.isAddOrEdit = false;
+        $scope.newProduct = {};
+        $scope.isNewProduct = true;
+    }
+});
+
+
+myApp.controller('employeeController', function () {
+    var vm = this;
+
+    vm.Message = "Test Controller";
+    vm.greet = function () {
+        alert('Hello..');
+    }
+
+});
+
+myApp.controller('indexController', function () {
+    var vm = this;
+    vm.Message = "Hello..Index Controller";
+
 });

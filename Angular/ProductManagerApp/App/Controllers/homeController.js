2b83285f267c77acc788be5312ad729544634b9c
dotnetsatya <dotnetsatya@gmail.com>
2016-11-15 20:33:59 -0500

    angular 4
    angular 4

--- Angular/ProductManagerApp/App/Controllers/homeController.js
+++ Angular/ProductManagerApp/App/Controllers/homeController.js
@@ -1,3 +1,9 @@
-﻿app.controller('homeController', function ($scope) {
+﻿app.controller('homeController', function ($scope,productFactory) {
+    $scope.Message = "hello..world";
 
+    var init = function () {
+        console.log(productFactory.testMethod());
+    }
+
+    init();
 });
